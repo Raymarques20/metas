cb88e6fb7603186f510c95ea9efb6b8b204cfd48
Raymarques20 <105437908+Raymarques20@users.noreply.github.com>
2024-04-11 08:33:42 -0300

    ok

--- main.js
+++ main.js
@@ -17,7 +17,11 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2023-12-05T00:00:00");
+const tempoObjetivo4 = new Date("2023-02-05T00:00:00");
 
+cont tempos =[tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4]:
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
@@ -33,5 +37,8 @@ function calculaTempo(tempoObjetivo) {
     minutos %= 60;
     horas %= 24;
  
+    if(tempoFinal > 0){
  return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
-}
+}else{
+     return " Prazo encerrado";
+    }
